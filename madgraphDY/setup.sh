44c44d7fe8751a7c7696936b120b816949d1bea8
rtschmitz <schmitz@ucsb.edu>
2024-09-25 14:41:54 -0700

    Updated setup to use updated madgraph

--- madgraphDY/setup.sh
+++ madgraphDY/setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 # latest="https://launchpad.net/mg5amcnlo/2.0/2.6.x/+download/MG5_aMC_v2.6.5.tar.gz"
-latest="https://launchpad.net/mg5amcnlo/2.0/2.6.x/+download/MG5_aMC_v2.6.7.tar.gz"
+latest="https://launchpad.net/mg5amcnlo/3.0/3.5.x/+download/MG5_aMC_v2.9.20.tar.gz"
 
 export FTINTBASE="$( cd "$(dirname "$BASH_SOURCE")" ; pwd -P )"
 
@@ -23,3 +23,4 @@ fi
 
 export MGEXE=$(ls -1 MG5*/bin/mg5_aMC | tail -n 1)
 alias run="./$MGEXE"
+
